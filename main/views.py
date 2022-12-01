340f2bd56ccce6af208f98c7dd5ea3864e8dbba2
Dung Lê Thị Thùy <lethithuydung2403@gmail.com>
2022-12-01 17:35:16 +0700

    done result

--- main/views.py
+++ main/views.py
@@ -14,30 +14,34 @@ def file_upload_optimize(request):
     if request.method == "POST":
         FileUploadForm(request.POST, request.FILES)
         handle_uploaded_file(request.FILES['file'])
-        fixed_length, (cutting_instruction_list, optimal_value) = get_cutting_instruction_by_uploading_file()
+        fixed_length, (cutting_instruction_list,
+                       optimal_value) = get_cutting_instruction_by_uploading_file()
+        list_color = ['red', 'blue', 'green', 'yellow', 'pink',
+                      'purple', 'orange', 'brown', 'black']
+
         # Define context for rendering
         response_context = {'fixed_length': int(fixed_length), 'optimal_value': int(optimal_value),
-             'cutting_instruction_list':  [{'number': int(cutting_instruction['number']), 'type': [ int(x) for x in cutting_instruction['type'] ], 'residual': int(cutting_instruction['residual']) } for cutting_instruction in cutting_instruction_list]}
+                            'cutting_instruction_list':  [{'number': int(cutting_instruction['number']), 'type': [{'value': int(x), 'color': list_color[cutting_instruction['type'].index(x) % len(list_color)]} for x in cutting_instruction['type']], 'residual': int(cutting_instruction['residual'])} for cutting_instruction in cutting_instruction_list],
+                            }
         # Path to CWD
         cwd = os.getcwd()
-        path_template_result = os.path.join(cwd, 'main','templates', 'main', 'result.html')
-        
-        print()
+        path_template_result = os.path.join(
+            cwd, 'main', 'templates', 'main', 'result.html')
         # Render the result page
         return render(request=request, template_name=path_template_result, context=response_context)
 
-        
-        
 
 def click_optimize(request):
     properties = dict(request.POST)
     print(properties)
-    fixed_length, (cutting_instruction_list, optimal_value) = get_cutting_instruction_by_filling(properties)
+    fixed_length, (cutting_instruction_list,
+                   optimal_value) = get_cutting_instruction_by_filling(properties)
     # Define context for rendering
     response_context = {'fixed_length': fixed_length, 'optimal_value': optimal_value,
-         'cutting_instruction_list': cutting_instruction_list}
+                        'cutting_instruction_list': cutting_instruction_list}
     # Path to CWD
     cwd = os.getcwd()
-    path_template_result = os.path.join(cwd, 'main','templates', 'main', 'result.html')
+    path_template_result = os.path.join(
+        cwd, 'main', 'templates', 'main', 'result.html')
     # Render the result page
     return render(request=request, template_name=path_template_result, context=response_context)
